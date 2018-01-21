1d59afdc039c14a2acc5b725d12f01c27d53097d
Petrica Andrei-Daniel <danielpetrica@users.noreply.github.com>
2018-01-21 14:33:03 +0100

    Create solution.py

---
+++ re_id/solution.py
@@ -0,0 +1,29 @@
+def answer(n):
+    numList = ""
+    num = 2
+    while numList.__len__() <= n + 5:
+        if is_prime(num):
+            numList += num.__str__()
+            #print(num)
+        num += 1
+
+    b = n + 5
+    return numList[n:b]
+
+
+def is_prime(n):
+    if n == 2 or n == 3: return True
+    if n < 2 or n % 2 == 0: return False
+    if n < 9: return True
+    if n % 3 == 0: return False
+    r = int(n ** 0.5)
+    f = 5
+    while f <= r:
+        # print '\t', f
+        if n % f == 0: return False
+        if n % (f + 2) == 0: return False
+        f += 6
+    return True
+
+
+print(answer(0))
